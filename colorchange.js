65b8f5b8d906227f9d6cef07d08f6f4e662930ae
devanshV18 <devanshverma024@gmail.com>
2023-10-02 05:53:47 +0530

    Project1 complete

---
+++ colorchange.js
@@ -0,0 +1,19 @@
+const btn1 = document.getElementById('b1')
+btn1.addEventListener('click', function onClick(event){
+    document.body.style.backgroundColor = 'grey'
+})
+
+const btn2 = document.getElementById('b2')
+btn2.addEventListener('click', function onClick(event){
+    document.body.style.backgroundColor = 'white'
+})
+
+const btn3 = document.getElementById('b3')
+btn3.addEventListener('click', function onClick(event){
+    document.body.style.backgroundColor = 'blue'
+})
+
+const btn4 = document.getElementById('b4')
+btn4.addEventListener('click', function onClick(event){
+    document.body.style.backgroundColor = 'yellow'
+})
